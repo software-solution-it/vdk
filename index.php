7d894b7277abec9e6d00a3881c12b48493948145
Seu Nome <seuemail@dominio.com>
2024-10-17 00:36:56 -0300

    ajustes caminho

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 use App\Controllers\AuthController;
 use App\Controllers\UserController;
 use App\Controllers\EmailController;
