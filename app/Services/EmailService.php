0afd1aab89db361c01ee3fa8c72d981a34d99e2b
Seu Nome <seuemail@dominio.com>
2024-11-06 11:25:14 -0300

    ajustes

--- app/Services/EmailService.php
+++ app/Services/EmailService.php
@@ -139,7 +139,7 @@ class EmailService {
             $mail->SMTPAuth   = true;
             $mail->Username   = $smtpConfig['email'];
             $mail->Password   = $smtpConfig['password'];
-            $mail->SMTPSecure = $smtpConfig['encryption'];
+            $mail->SMTPSecure = 'ssl';
             $mail->Port       = $smtpConfig['smtp_port'];
     
             $mail->setFrom($smtpConfig['email'], $smtpConfig['name']);
@@ -328,7 +328,7 @@ class EmailService {
             $mail->SMTPAuth   = true;
             $mail->Username   = $smtpConfig['email'];
             $mail->Password   = $smtpConfig['password'];
-            $mail->SMTPSecure = $smtpConfig['encryption'];
+            $mail->SMTPSecure = 'ssl';
             $mail->Port       = $smtpConfig['smtp_port'];
 
             $mail->setFrom($smtpConfig['email'], $smtpConfig['name']);
