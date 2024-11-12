3c1c30fd7fa0b65e3de06151cc006b1b86b21d2d
Seu Nome <seuemail@dominio.com>
2024-11-12 06:42:24 -0300

    ajustes

--- app/Scripts/run_email_workers.sh
+++ app/Scripts/run_email_workers.sh
@@ -1,34 +1,25 @@
-LOCK_DIR="/tmp/email_sync_locks"
-mkdir -p "${LOCK_DIR}"
-
 DATABASE_USER="root" 
 DATABASE_PASS="root" 
 DATABASE_NAME="mail"
 
+
 EMAIL_LIST=$(mysql -u ${DATABASE_USER} -p${DATABASE_PASS} -D ${DATABASE_NAME} -sse "SELECT user_id, id as email_id FROM email_accounts;")
 
 while IFS=$'\t' read -r user_id email_id; do
-    LOCK_FILE="${LOCK_DIR}/email_sync_${email_id}.lock"
+    LOCK_FILE="/tmp/email_sync_${email_id}.lock"
 
-    if [ -e "${LOCK_FILE}" ]; then
-        echo "Processo de sincronização já em execução para email_id=${email_id}. Ignorando..."
-        continue
-    fi
+    # Usar flock para garantir que apenas um processo por email_id seja executado ao mesmo tempo
+    {
+        flock -n 200 || {
+            echo "Processo de sincronização já em execução para email_id=${email_id}. Ignorando..."
+            continue
+        }
 
-    # Cria o lock file
-    touch "${LOCK_FILE}"
+        echo "Iniciando worker para user_id=${user_id} e email_id=${email_id}"
+        ${PHP_BIN} ${WORKER_SCRIPT} ${user_id} ${email_id} > /dev/null 2>&1
 
-    echo "Iniciando worker para user_id=${user_id} e email_id=${email_id}"
-    ${PHP_BIN} ${WORKER_SCRIPT} ${user_id} ${email_id} > /dev/null 2>&1 &
-
-    # Salvar o PID do processo no lock file para controle
-    echo $! > "${LOCK_FILE}"
-
-    # Usar um subshell para esperar pelo término do processo e então remover o lock file
-    (
-        pid=$(<"${LOCK_FILE}")
-        wait "${pid}"
-        rm -f "${LOCK_FILE}"
-    ) &
+    } 200>"${LOCK_FILE}" &
 
 done <<< "$EMAIL_LIST"
+
+wait
