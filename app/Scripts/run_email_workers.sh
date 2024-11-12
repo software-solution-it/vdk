8c74ae00817f278ef90295822f301f8023d846a4
Seu Nome <seuemail@dominio.com>
2024-11-12 07:43:43 -0300

    ajustes

--- app/Scripts/run_email_workers.sh
+++ app/Scripts/run_email_workers.sh
@@ -4,7 +4,8 @@ DATABASE_NAME="mail"
 PHP_BIN="/usr/bin/php"
 WORKER_SCRIPT="/home/suporte/vdk/app/Worker/email_sync_worker.php"
 
-# Função que sincroniza o e-mail em loop contínuo
+declare -A active_workers
+
 sync_email_in_loop() {
     local user_id=$1
     local email_id=$2
@@ -12,15 +13,12 @@ sync_email_in_loop() {
 
     while true; do
         {
-            # Tenta adquirir o lock para o email_id
             if flock -n 200; then
                 echo "Iniciando worker para user_id=${user_id} e email_id=${email_id}"
                 ${PHP_BIN} ${WORKER_SCRIPT} ${user_id} ${email_id} > /dev/null 2>&1
                 
-                # Quando o processo terminar, o lock será automaticamente liberado pelo `flock`
                 echo "Worker finalizado para user_id=${user_id} e email_id=${email_id}. Reiniciando..."
             else
-                # Se o lock já estiver sendo usado, aguarda antes de tentar novamente
                 echo "Worker já em execução para email_id=${email_id}. Aguardando para reiniciar..."
                 sleep 5
             fi
@@ -28,13 +26,15 @@ sync_email_in_loop() {
     done
 }
 
-# Obter a lista de emails do banco de dados
-EMAIL_LIST=$(mysql -u ${DATABASE_USER} -p${DATABASE_PASS} -D ${DATABASE_NAME} -sse "SELECT user_id, id as email_id FROM email_accounts;")
+while true; do
+    EMAIL_LIST=$(mysql -u ${DATABASE_USER} -p${DATABASE_PASS} -D ${DATABASE_NAME} -sse "SELECT user_id, id as email_id FROM email_accounts;")
 
-# Iterar sobre cada email para iniciar o processo de sincronização em loop
-while IFS=$'\t' read -r user_id email_id; do
-    sync_email_in_loop "${user_id}" "${email_id}" &
-done <<< "$EMAIL_LIST"
+    while IFS=$'\t' read -r user_id email_id; do
+        if [[ -z "${active_workers[${email_id}]}" || ! -e "/proc/${active_workers[${email_id}]}" ]]; then
+            sync_email_in_loop "${user_id}" "${email_id}" &
+            active_workers[${email_id}]=$!
+        fi
+    done <<< "$EMAIL_LIST"
 
-# Espera todos os processos em segundo plano terminarem antes de encerrar o script
-wait
+    sleep 15
+done
